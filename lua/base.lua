1bfa8ea33e9bf83afd4ff2fc7e2c12b728ab406b
Aaron <admin@niceboy.org>
2024-01-12 10:51:34 +0800

    base

--- lua/base.lua
+++ lua/base.lua
@@ -1,4 +1,30 @@
-local opt=vim.opt
+local opt = vim.opt
+local g = vim.g
 
-opt.number=true
-opt.relativenumber=true
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+opt.number = true
+opt.relativenumber = true
+
+opt.scrolloff = 5
+opt.sidescrolloff = 5
+
+opt.hlsearch = true
+opt.incsearch = true
+
+-- The tag netrw has been loaded
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+opt.termguicolors = true
